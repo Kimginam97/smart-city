04c82d79c1d3f7d07008abbcfda1080bda2324fe
Kimginam97 <rlarlska79@naver.com>
2024-02-15 13:24:33 +0900

    Ref Links.jsx

--- components/sidebar/components/Links.jsx
+++ components/sidebar/components/Links.jsx
@@ -1,20 +1,19 @@
-/* eslint-disable */
+import Link from 'next/link' // Next.js의 Link 컴포넌트 가져오기
+import { usePathname } from 'next/navigation' // Next.js의 usePathname 훅 가져오기
 
-import DashIcon from '@/components/icons/DashIcon'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-// chakra imports
+import DashIcon from '@/components/icons/DashIcon' // 대시 아이콘 컴포넌트 가져오기
 
+// 사이드바 링크 컴포넌트 정의
 export function SidebarLinks(props) {
-  // Chakra color mode
-  let location = usePathname()
-  const { routes } = props
+  const { routes } = props // 라우트 가져오기
+  let location = usePathname() // 현재 경로 가져오
 
-  // verifies if routeName is the one active (in browser input)
+  // 활성 경로인지 확인하는 함수
   const activeRoute = (routeName) => {
     return location.includes(routeName)
   }
 
+  // 링크 생성 함수
   const createLinks = (routes) => {
     return routes.map((route, index) => {
       if (route.layout === '/admin' || route.layout === '/auth') {
@@ -22,6 +21,7 @@ export function SidebarLinks(props) {
           <Link key={index} href={`${route.layout}/${route.path}`}>
             <div className="relative mb-3 flex hover:cursor-pointer">
               <li className="my-[3px] flex cursor-pointer items-center px-8" key={index}>
+                {/* 아이콘 */}
                 <span
                   className={`${
                     activeRoute(route.path) === true
@@ -29,8 +29,9 @@ export function SidebarLinks(props) {
                       : 'font-medium text-gray-600'
                   }`}
                 >
-                  {route.icon ? route.icon : <DashIcon />}{' '}
+                  {route.icon ? route.icon : <DashIcon />}
                 </span>
+                {/* 이름 */}
                 <p
                   className={`leading-1 ml-4 flex ${
                     activeRoute(route.path) === true
@@ -41,6 +42,7 @@ export function SidebarLinks(props) {
                   {route.name}
                 </p>
               </li>
+              {/* 활성화 선 */}
               {activeRoute(route.path) ? (
                 <div className="absolute right-0 top-px h-9 w-1 rounded-lg bg-brand-500 dark:bg-brand-400" />
               ) : null}
@@ -50,8 +52,8 @@ export function SidebarLinks(props) {
       }
     })
   }
-  // BRAND
+  // 링크 생성하여 반환
   return createLinks(routes)
 }
 
-export default SidebarLinks
+export default SidebarLinks // 사이드바 링크 컴포넌트 내보내기
